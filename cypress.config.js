61acffb9e03b1cf2832197e29d283fe623582fd6
ekosy9 <ekosyaepulloh9@gmail.com>
2025-05-08 20:56:05 +0700

    add mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,22 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    browser : 'chrome',
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    reporter: 'mochawesome', 
+    reporterOptions: {
+      reportDir: 'cypress/results', // results will be saved in this folder for html and json report
+      overwrite: false,             
+      html: true,                   
+      json: true                    
+    },
+    video: true, 
+    videoCompression: 32, // Set the video compression quality (value between 0-51)
+    videosFolder: 'cypress/videos', 
+    screenshotOnRunFailure: true, 
+    screenshotsFolder: 'cypress/screenshots', 
   },
 });
