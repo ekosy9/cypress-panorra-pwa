2e2df3c8e2272b7856e93f33fe5880b612c96694
ekosy9 <ekosyaepulloh9@gmail.com>
2025-05-08 21:03:49 +0700

    test

---
+++ cypress/e2e/integration/1-authentication/1_1-register-account.cy copy.js
@@ -0,0 +1,16 @@
+describe('Login Page Feature', () => {
+  it('Login valid', () => {
+    cy.viewport(1920, 1080);
+    cy.visit('https://panorra.com/');
+    cy.wait(5000);
+    cy.get('.btn.btn-primary.btn-sm.responsive-hide').click();
+    cy.get('input[placeholder="Enter your email or username"]',{ timeout: 60000 })
+    .should('be.visible')
+    .type('testergothru@gmail.com');
+    cy.get('input[placeholder="Enter your password"]',{ timeout: 60000 })
+    .should('be.visible')
+    .type('gothru@12345');     
+    cy.get('div.login').find('button').contains('Log In').click();
+    cy.wait(5000);
+  })
+})
